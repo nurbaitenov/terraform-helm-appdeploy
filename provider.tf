026e746cf6124ab4358ec45ad0532014ed2f0408
nurbaitenov <baitenovnur@gmail.com>
2026-08-11 20:05:21 +0000

    removed localhost 5000

--- provider.tf
+++ provider.tf
@@ -2,17 +2,4 @@ provider "helm" {
   kubernetes = {
     config_path = "~/.kube/config"
   }
-
-  registries = [
-    {
-      url      = "oci://localhost:5000"
-      username = "username"
-      password = "password"
-    },
-    {
-      url      = "oci://private.registry"
-      username = "username"
-      password = "password"
-    }
-  ]
 }
